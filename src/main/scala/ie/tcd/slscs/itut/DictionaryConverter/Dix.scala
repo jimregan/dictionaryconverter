549419baf9e4d9dbf12e217211af83f698e53c37
Jim O'Regan <joregan@gmail.com>
2017-07-06 23:44:03 +0100

    more

--- src/main/scala/ie/tcd/slscs/itut/DictionaryConverter/Dix.scala
+++ src/main/scala/ie/tcd/slscs/itut/DictionaryConverter/Dix.scala
@@ -39,17 +39,19 @@ class Dictionary(alphabet: String, sdefs: List[Sdef], pardefs: List[Pardef], sec
   }
 }
 
+abstract class DixElement {
+  def toXML
+}
+
 case class Sdef(n: String, c: String = null) {
   def toXML = <sdef n={n} c={c} />
 }
 
-abstract class TextLike {
-  def toXML
-}
+trait TextLike extends DixElement
 case class B extends TextLike {
   def toXML = <b/>
 }
-case class Text(s: String) extends TextLike {
+case class Txt(s: String) extends TextLike {
   def toXML = new scala.xml.Text(s)
 }
 case class S(n: String) extends TextLike {
@@ -78,18 +80,16 @@ case class E(children: List[Parts], lm: String = null) {
   }
 }
 
-abstract class Parts() {
-  def toXML
-}
+abstract class Parts() extends DixElement
 
-abstract class TextLikeContainer(content: List[TextLike])
+abstract class TextLikeContainer(content: List[TextLike]) extends DixElement
 case class L(content: List[TextLike]) extends TextLikeContainer(content) {
   def toXML = <l>{ content.map{c => c.toXML} }</l>
 }
 case class R(content: List[TextLike]) extends TextLikeContainer(content) {
   def toXML = <r>{ content.map{c => c.toXML} }</r>
 }
-case class G(content: List[TextLike]) extends TextLikeContainer(content) {
+case class G(content: List[TextLike]) extends TextLikeContainer(content) with TextLike {
   def toXML = <g>{ content.map{c => c.toXML} }</g>
 }
 case class Par(name: String, sa: String = null, prm: String = null) extends TextLikeContainer(List[TextLike]()) {
@@ -105,10 +105,32 @@ case class P(l: L, r: R) extends TextLikeContainer(List[TextLike]()) {
   def toXML = <p><l>{l.toXML}</l><r>{r.toXML}</r></p>
 }
 
-
 object Dictionary {
   import scala.xml.XML
-//  def readcontent
+  def nodetocontent(node: Node): TextLike = node match {
+    case scala.xml.Text(t) => Txt(t)
+    case <b/> => B()
+    case s @ <s/> => S(s.attribute("n").get(0).text)
+    case _ => throw new Exception("Error reading sdef")
+  }
+  def nodetocontainer(node: Node): TextLikeContainer = node match {
+    case <i>{cnt @ _*}</i> => I(cnt.toList.map{nodetocontent})
+    case <p><l>{l @ _*}</l><r><g>{g @ _*}</g></r></p> => P(L(l.toList.map{nodetocontent}), R(List[TextLike](G(g.toList.map{nodetocontent}))))
+    case <p><l>{l @ _*}</l><r>{r @ _*}</r></p> => P(L(l.toList.map{nodetocontent}), R(r.toList.map{nodetocontent}))
+    case par @ <par/> => {
+      val satxt = par.attribute("sa").get(0).text
+      val sa = if (satxt != "") satxt else null
+      val prmtxt = par.attribute("prm").get(0).text
+      val prm = if (prmtxt != "") prmtxt else null
+      val name = par.attribute("n").get(0).text
+      Par(name, sa, prm)
+    }
+    case <re>{re}</re> => RE(re.text)
+    case <g>{foo @ _*}</g> => throw new Exception("Error reading <g>: cannot appear outside of <r>")
+    case <l>{foo @ _*}</l> => throw new Exception("Error reading <l>: cannot appear outside of <p>")
+    case <r>{foo @ _*}</r> => throw new Exception("Error reading <r>: cannot appear outside of <p>")
+    case _ => throw new Exception("Error reading e")
+  }
   def nodetosdef(node: Node): Sdef = node match {
     case <sdef/> => {
       val name = node \ "@n"
