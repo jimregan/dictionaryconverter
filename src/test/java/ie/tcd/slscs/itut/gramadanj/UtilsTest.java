9236f7f4e9354826adde0a14a070320ee4abdbb1
Jim O'Regan <joregan@gmail.com>
2017-07-24 01:29:40 +0100

    add java

---
+++ src/test/java/ie/tcd/slscs/itut/gramadanj/UtilsTest.java
@@ -0,0 +1,76 @@
+package ie.tcd.slscs.itut.gramadanj;
+/*
+ * Copyright © 2016 Trinity College, Dublin
+ * Irish Speech and Language Technology Research Centre
+ * Cóipcheart © 2016 Coláiste na Tríonóide, Baile Átha Cliath
+ * An tIonad taighde do Theicneolaíocht Urlabhra agus Teangeolaíochta na Gaeilge
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+import junit.framework.TestCase;
+import ie.tcd.slscs.itut.gramadanj.Utils;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+
+public class UtilsTest extends TestCase {
+    public void testExpandParentheticalVariants() throws Exception {
+        String testin = "colo(u)r";
+        String[] testout = {"color", "colour"};
+        String[] run = Utils.expandParentheticalVariants(testin);
+        assertArrayEquals(testout, run);
+    }
+
+    public void testCleanTrailingPunctuation() throws Exception {
+        final String ina = "thing,";
+        final String inb = "thing";
+        assertEquals(inb, Utils.cleanTrailingPunctuation(ina));
+        assertEquals(inb, Utils.cleanTrailingPunctuation(inb));
+    }
+
+    public void testEqualLists() {
+        List<String> a = new ArrayList<String>();
+        List<String> b = new ArrayList<String>();
+        a.add("bee");
+        a.add("cee");
+        a.add("ay");
+        b.add("cee");
+        b.add("ay");
+        b.add("bee");
+        assertEquals(true, Utils.equalLists(a, b));
+    }
+
+    public void testTrim() {
+        assertEquals("", Utils.trim("     "));
+        assertEquals("aaa", Utils.trim("  aaa   "));
+        assertEquals("aaa", Utils.trim("  aaa"));
+        assertEquals("aaa", Utils.trim("aaa  "));
+    }
+
+    public void testExpandFGB() {
+        assertEquals("endings", Utils.expandFGB("ending", "~s"));
+        assertEquals("chasing", Utils.expandFGB("chase", "-sing"));
+        assertEquals("truncálann", Utils.expandFGB("truncáil", "-álann"));
+    }
+}
