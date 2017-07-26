13aa7487010dd684e684f823930f2157b15335db
Jim O'Regan <jaoregan@tcd.ie>
2017-07-26 11:55:20 +0100

    more trx reading

--- src/main/scala/ie/tcd/slscs/itut/DictionaryConverter/Trx.scala
+++ src/main/scala/ie/tcd/slscs/itut/DictionaryConverter/Trx.scala
@@ -27,6 +27,20 @@ import scala.xml._
 
 trait TransferElement {
   def toXML: scala.xml.Node
+  def toXMLString = toXML.toString
+}
+case class TopLevel(kind: String, defcats: List[DefCat], defattrs: List[AttrCat], vars: List[DefVar], lists: List[DefList], macros: List[DefMacro], rules: List[Rule]) extends TransferElement {
+  def getOpen: String = if(kind == "chunk") {
+    "<transfer default=\"chunk\">"
+  } else {
+    "<" + kind + ">"
+  }
+  def getClose: String = if(kind == "chunk") {
+    "</transfer>"
+  } else {
+    "</" + kind + ">"
+  }
+  def toXML = <FIXME/>
 }
 case class CatItem(tags: String, lemma: String = null) extends TransferElement {
   def toXML = <cat-item lemma={lemma} tags={tags} />
@@ -51,7 +65,41 @@ case class AttrCat(n: String, l: List[AttrItem]) extends TransferElement {
 case class DefVar(name: String, value: String = null) extends TransferElement {
   def toXML = <def-var n={name} v={value} />
 }
+case class Rule(comment: String, pattern: List[PatternItem], action: Action)
+case class PatternItem(n: String)
+case class Action(c: List[Sentence]) extends TransferElement {
+  def toXML = <action>
+    { c.map{_.toXML} }
+  </action>
+}
+trait Condition extends TransferElement
+trait Container extends TransferElement
+trait Sentence extends TransferElement
+trait Value extends TransferElement
+trait StringValue extends Value
+case class Var(name: String) extends Container with StringValue {
+  def toXML = <var n={name}/>
+}
+case class DefMacro(name: String, numparams: String, comment: String, actions: List[Action]) extends TransferElement {
+  override def toXML: Node = <def-macro n={name} npar={numparams} c={comment}>
+    { actions.map{_.toXML} }
+  </def-macro>
+}
+case class DefList(name: String, items: List[ListItem]) extends TransferElement {
+  override def toXML: Node = <def-list n={name}>
+    { items.map{_.toXML} }
+  </def-list>
 
+  override def toXMLString: String = s"""
+    <def-list n="$name">
+${ items.map{_.toXMLString} }
+    </def-list>
+"""
+}
+case class ListItem(value: String) extends TransferElement {
+  def toXML = <list-item v={value}/>
+  override def toXMLString = "      " + toXML.toString() + "\n"
+}
 
 object Trx {
   import scala.xml._
@@ -65,5 +113,35 @@ object Trx {
     val name = (n \ "@n").text
     val children = (n \ "cat-item").toList.map{nodeToCatItem}
     DefCat(name, children)
+  }
+  def nodeToVar(n: Node): Var = {
+    val name = (n \ "@n").text
+    Var(name)
+  }
+  def nodeToAttrItem(n: Node): AttrItem = {
+    val tags = (n \ "@tags").text
+    AttrItem(tags)
+  }
+  def nodeToDefAttr(n: Node): AttrCat = {
+    val name = (n \ "@n").text
+    val items = (n \ "attr-item").map{nodeToAttrItem}.toList
+    AttrCat(name, items)
+  }
+
+  def load(file: String): TopLevel = {
+    val xml = XML.loadFile(file)
+    def rootElementToTrxType(e: Elem): String = {
+      val valid = List("transfer", "interchunk", "postchunk")
+      if (e.label == "transfer" && e.attribute("default").get.text == "chunk") {
+        "chunk"
+      } else if (valid.contains(e.label)) {
+        e.label
+      } else {
+        throw new Exception("File " + file + " not recognised as TRX: top-level element is " + e.label)
+      }
+    }
+    val kind = rootElementToTrxType(xml)
+    val defcats = (xml \ "section-def-cats" \ "def-cat").map{nodeToDefCat}
+    val defattrs = (xml \ "section-def-attrs" \ "def-attr").map{nodeToDefAttr}
   }
 }
