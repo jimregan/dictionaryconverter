5c44439c46cda1898ae83d04c44c29b7ecc5a0dd
Jim O'Regan <jaoregan@tcd.ie>
2017-08-01 13:16:36 +0100

    start

---
+++ src/main/java/ie/tcd/slscs/itut/ApertiumStream/MLUToken.java
@@ -0,0 +1,54 @@
+/*
+ *  The MIT License (MIT)
+ *
+ *  Copyright © 2017 Trinity College, Dublin
+ *  Irish Speech and Language Technology Research Centre
+ *  Cóipcheart © 2017 Coláiste na Tríonóide, Baile Átha Cliath
+ *  An tIonad taighde do Theicneolaíocht Urlabhra agus Teangeolaíochta na Gaeilge
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package ie.tcd.slscs.itut.ApertiumStream;
+
+import ie.tcd.slscs.itut.gramadanj.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MLUToken extends StreamToken {
+    List<WordToken> lus;
+    MLUToken() {
+        lus = new ArrayList<WordToken>();
+    }
+
+    @Override
+    public String toString() {
+        return "^" + getContent() + "$";
+    }
+
+    @Override
+    public String getContent() {
+        List<String> tmp = new ArrayList<String>();
+        for (WordToken lu : lus) {
+            tmp.add(lu.getContent());
+        }
+        return Utils.join(tmp, "+");
+    }
+}
