fcaaf1350df187005d55d991f540c5c27497ebdf
Jim O'Regan <jaoregan@tcd.ie>
2017-08-01 22:24:26 +0100

    the actual problem

--- src/main/scala/ie/tcd/slscs/itut/DictionaryConverter/TrxUtils.scala
+++ src/main/scala/ie/tcd/slscs/itut/DictionaryConverter/TrxUtils.scala
@@ -58,7 +58,7 @@ object TrxUtils {
 
 //  }
   def isSimpleEntry(entry: E): Boolean = entry.children match {
-    case P(L(_), R(_)) => true
+    case P(_, _, _) :: nil => true
     case _ => false
   }
 //  def dixEntryToWhen(entry: E): WhenElement = {
