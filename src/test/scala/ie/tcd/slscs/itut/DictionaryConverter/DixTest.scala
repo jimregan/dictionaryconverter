43dcc23e92b64e552dac0255f06789fa301b868b
Jim O'Regan <jaoregan@tcd.ie>
2017-08-04 09:41:58 +0100

    start test

--- src/test/scala/ie/tcd/slscs/itut/DictionaryConverter/DixTest.scala
+++ src/test/scala/ie/tcd/slscs/itut/DictionaryConverter/DixTest.scala
@@ -33,9 +33,35 @@ import ie.tcd.slscs.itut.DictionaryConverter.EID._
 import ie.tcd.slscs.itut.DictionaryConverter.dix.{Dix, E, I, Txt}
 
 class DixTest extends FlatSpec {
+  val simple_dix = <dictionary>
+    <alphabet>abcdefghijklmonpqrstuvwxyz</alphabet>
+    <sdefs>
+      <sdef n="one"/>
+      <sdef n="two" c="comment" />
+    </sdefs>
+    <pardefs>
+      <pardef n="first">
+        <e><i>foo</i></e>
+        <e><p><l/><r><s n="one"/></r></p></e>
+      </pardef>
+      <pardef n="second">
+        <e><p><l>foo</l><r>bar<s n="one"/></r></p></e>
+      </pardef>
+    </pardefs>
+    <section id="main" type="standard">
+      <e><p><l>foo</l><r>bar<s n="one"/></r></p></e>
+      <e><p><l>bar</l><r>barbar<s n="one"/></r></p><par n="first"/></e>
+      <e><p><l>foo</l><r>bars<s n="one"/></r></p><par n="second"/></e>
+    </section>
+  </dictionary>
+
   "e" should "read <e> element" in {
     val xml = <e><i>txt</i></e>
     val prs = E(List(I(List(Txt("txt")))))
     assert(prs == Dix.nodetoe(xml))
+  }
+
+  "whole" should "read whole dictionary" in {
+    val dix = Dix.load(simple_dix)
   }
 }
