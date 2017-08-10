9597acadb0ef297b5c9c0b7adaea5690eeaee9a8
Jim O'Regan <jaoregan@tcd.ie>
2017-08-10 20:39:42 +0100

    also wrap exp

--- src/test/scala/ie/tcd/slscs/itut/DictionaryConverter/EIDTest.scala
+++ src/test/scala/ie/tcd/slscs/itut/DictionaryConverter/EIDTest.scala
@@ -66,14 +66,14 @@ class EIDTest extends FlatSpec {
 
   "read complex trg" should "read trg with complex contents" in {
     val in = <entry><trg>Crios <label>m</label> (<label>g.</label> creasa)</trg></entry>
-    val exp = MultiTrg(List(Txt("Crios "), Label("m"), Txt(" ("), Label("g."), Txt(" creasa)")))
+    val exp = List[BaseXML](MultiTrg(List(Txt("Crios "), Label("m"), Txt(" ("), Label("g."), Txt(" creasa)"))))
     val out = breakdownComplexEntry(in)
     assert(exp == out)
   }
 
   "read simple src" should "read src element" in {
     val in = <entry><src>foo</src></entry>
-    val exp = Src("foo")
+    val exp = List[BaseXML](Src("foo"))
     val out = breakdownComplexEntry(in)
     assert(exp == out)
   }
