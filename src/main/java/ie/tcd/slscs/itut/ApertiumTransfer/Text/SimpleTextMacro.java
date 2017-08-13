820d7b6eb7c1e8ccd034d7699697a15fce7f97d3
Jim O'Regan <jaoregan@tcd.ie>
2017-08-13 22:57:40 +0100

    create

---
+++ src/main/java/ie/tcd/slscs/itut/ApertiumTransfer/Text/SimpleTextMacro.java
@@ -0,0 +1,69 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright © 2017 Trinity College, Dublin
+ * Irish Speech and Language Technology Research Centre
+ * Cóipcheart © 2017 Coláiste na Tríonóide, Baile Átha Cliath
+ * An tIonad taighde do Theicneolaíocht Urlabhra agus Teangeolaíochta na Gaeilge
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package ie.tcd.slscs.itut.ApertiumTransfer.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * As a macro can contain multiple operations, this groups the parts
+ * together, and contains the common parts: name, and tokens the
+ * macro applies to, for use in generating the number of expected
+ * parameters, and for matching the macro to the rule it can
+ * apply to.
+ *
+ * For example:
+ * <pre>
+ * adj_grade | <adj> | <comp> | níos<adv> <comp> | 0-1 1-2
+ *  | | <sup> | is<adv> <comp> | 0-1 1-2
+ * </pre>
+ * The macro "adj_grade" applies to &lt;adj&gt; and has two actions:
+ * one which inserts "níos" in front of the adjective, the other
+ * which inserts "is", and sets the tag from sup to comp.
+ * Insertion is marked with a 0 in the alignment piece.
+ * FIXME: change notation to &lt;grade=comp&gt; to mark what is
+ * being compared? Multiple attributes could contain these tags.
+ * Example 2:
+ * <pre>
+ * det_type | <det> | no<det> | <negative=NEG> | 1-C
+ * | | the<det> | <det_type=DEFART> | 1-C
+ * | | a<det> | <det_type=UNDET> | 1-C
+ * </pre>
+ *
+ * Here, all of the operations are to be applied to the chunk lemma,
+ * which is denoted by 'C' in the alignment part.
+ */
+public class SimpleTextMacro {
+    String name;
+    List<String> appliesTo;
+    List<SimpleTextMacroPart> parts;
+    SimpleTextMacro() {
+        appliesTo = new ArrayList<String>();
+        parts = new ArrayList<SimpleTextMacroPart>();
+    }
+}
