1075efcd800c3d631440ef0b2b7431b049124b12
Jim O'Regan <jaoregan@tcd.ie>
2017-08-20 20:13:42 +0100

    toString

--- src/main/java/ie/tcd/slscs/itut/ApertiumStream/PositionedBlank.java
+++ src/main/java/ie/tcd/slscs/itut/ApertiumStream/PositionedBlank.java
@@ -42,4 +42,13 @@ public class PositionedBlank extends BlankToken {
     public void setPosition(int position) {
         this.position = position;
     }
+    @Override
+    public String getContent() {
+        return Integer.toString(position);
+    }
+
+    @Override
+    public String toString() {
+        return getContent();
+    }
 }
