ff0b9c32135e74dbd12adff4a476a363c539e827
Jim O'Regan <jaoregan@tcd.ie>
2017-08-21 02:01:02 +0100

    out?

--- src/test/java/ie/tcd/slscs/itut/ApertiumTransfer/Text/AlignmentPairTest.java
+++ src/test/java/ie/tcd/slscs/itut/ApertiumTransfer/Text/AlignmentPairTest.java
@@ -77,6 +77,7 @@ public class AlignmentPairTest extends TestCase {
         AlignmentPair b = new AlignmentPair("4", "1");
         AlignmentPair exp = new AlignmentPair("4", "2");
         AlignmentPair out = AlignmentPair.offsetPair(a, b);
+        if(out == null) throw new Exception(" out ");
         if(out.left == null || out.right == null) {
             throw new Exception("failed to run test: \"" + a + "\" \"" + b + "\"");
         }
