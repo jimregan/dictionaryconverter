39c9d2ede2a1023a1a3793e282a106d9696b093e
Jim O'Regan <jaoregan@tcd.ie>
2017-08-23 11:07:00 +0100

    throw if negates & has content

--- src/main/java/ie/tcd/slscs/itut/lex/CharGroup.java
+++ src/main/java/ie/tcd/slscs/itut/lex/CharGroup.java
@@ -78,6 +78,9 @@ public class CharGroup extends Grouping {
             String content = "";
             if(n.getChildNodes().getLength() == 1 && n.getChildNodes().item(0).getNodeName().equals("#text")) {
                 content = n.getChildNodes().item(0).getTextContent();
+                if(negates != null || !negates.equals("")) {
+                    throw new Exception("chargroup " + name + " contains unexpected content");
+                }
             }
             CharGroup out = new CharGroup(name);
             out.setNegates(negates);
