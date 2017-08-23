e4603b519c66471cd0c1c2f58c697ec98bfd234d
Jim O'Regan <jaoregan@tcd.ie>
2017-08-23 15:05:52 +0100

    duuuuh

--- src/test/java/ie/tcd/slscs/itut/lex/GroupTest.java
+++ src/test/java/ie/tcd/slscs/itut/lex/GroupTest.java
@@ -27,8 +27,10 @@
 
 package ie.tcd.slscs.itut.lex;
 
+import ie.tcd.slscs.itut.gramadanj.Utils;
 import junit.framework.TestCase;
 import org.junit.Test;
+import org.w3c.dom.Node;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -56,8 +58,24 @@ public class GroupTest extends TestCase {
             "      <item>sh</item>\n" +
             "      <item>x</item>\n" +
             "    </group>\n";
-    public void setUp() {
 
+    Node innode1;
+    Node innode2;
+    Node innode3;
+    Node innode4;
+    PairGroup out1;
+    PairGroup out2;
+    PairGroup out3;
+    PairGroup out4;
+    public void setUp() throws Exception {
+        innode1 = Utils.stringToNode(in1);
+        innode2 = Utils.stringToNode(in2);
+        innode3 = Utils.stringToNode(in3);
+        innode4 = Utils.stringToNode(in4);
+        out1 = PairGroup.fromNode(innode1);
+        out2 = PairGroup.fromNode(innode2);
+        out3 = PairGroup.fromNode(innode3);
+        out4 = PairGroup.fromNode(innode4);
     }
 
     public void testGetRegex() throws Exception {
@@ -72,8 +90,32 @@ public class GroupTest extends TestCase {
         assertEquals("(ch|sh|x)", g.getRegex());
     }
 
-    public void testFromNode() throws Exception {
-
+    public void testFromNodeName() throws Exception {
+        assertEquals("add_e", out1.getName());
+    }
+    public void testFromNodeRepeated1() throws Exception {
+        assertEquals(false, out1.isRepeated());
+    }
+    public void testFromNodeOptional1() throws Exception {
+        assertEquals(false, out1.isOptional());
+    }
+    public void testFromNodeRepeated2() throws Exception {
+        assertEquals(true, out2.isRepeated());
+    }
+    public void testFromNodeOptional2() throws Exception {
+        assertEquals(false, out2.isOptional());
+    }
+    public void testFromNodeRepeated3() throws Exception {
+        assertEquals(false, out3.isRepeated());
+    }
+    public void testFromNodeOptional3() throws Exception {
+        assertEquals(true, out3.isOptional());
+    }
+    public void testFromNodeRepeated4() throws Exception {
+        assertEquals(true, out4.isRepeated());
+    }
+    public void testFromNodeOptional4() throws Exception {
+        assertEquals(true, out4.isOptional());
     }
 
 }
